7d3f5213f8735554409904e41b5e70b5290c4303
c0cde4un <code4un@yandex.ru>
2024-02-01 20:50:17 +0500

    Task1 Completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int    _age = 33;
+        string _name = "Денис";
+        float  _outsideTemperatureInCelcius = -8.0F;
+        bool   _isFemale = false;
+        Console.WriteLine("Возраст: " + _age);
+        Console.WriteLine("Имя: " + _name);
+        Console.WriteLine("Тепмература за окном: " + _outsideTemperatureInCelcius);
+        Console.WriteLine("Женского пола ?: " + _isFemale);
     }
 }
