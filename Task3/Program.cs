103b592f9f0b36df64ec064e543ed8a45a38e1e5
c0cde4un <code4un@yandex.ru>
2024-02-01 21:11:03 +0500

    #task3 Comple Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите первую фразу:");
+        string _phrase1 = Console.ReadLine() ?? "Привет";
+        Console.WriteLine("Введите вторую фразу:");
+        string _phrase2 = Console.ReadLine() ?? " Мир !";
+
+        string _completedText = _phrase1 + _phrase2;
+        Console.WriteLine(_completedText);
     }
 }
