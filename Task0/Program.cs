5fa674ee6f2582787950d177194c74a5b62fbb07
c0cde4un <code4un@yandex.ru>
2024-02-01 20:38:44 +0500

    Complete Task0

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int _age = 30;
+        string _name = "Денис";
+        string _company = "Arwin";
+        bool _isWorking = true;
+        float _employeeWeight = 100.0F;
+
+        Console.WriteLine("Возраст: " + _age);
+        Console.WriteLine("Имя: " + _name);
+        Console.WriteLine("Организация: " + _company);
+        Console.WriteLine("Работает: " + _isWorking.ToString());
+        Console.WriteLine("Вес: " + _employeeWeight);
     }
 }
