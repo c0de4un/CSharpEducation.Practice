e8cb3280ad6ff948c63511d8ab9ce698a6924d87
c0cde4un <code4un@yandex.ru>
2024-02-01 21:03:17 +0500

    Completed Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Привет !");
+        Console.WriteLine("Введите свое имя: ");
+        string _name = Console.ReadLine() ?? "He указано";
+        Console.WriteLine("Вы ввели имя: " + _name);
     }
 }
