3eb4602eda3b4766d2d337cd45d96888d291ab3d
c0cde4un <code4un@yandex.ru>
2024-03-07 19:43:09 +0400

    Implement Task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("For: ");
+        for (int i = 0; i < 10; i++) {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("While: ");
+        int _counter = 0;
+        while (_counter < 10) {
+            Console.WriteLine(_counter);
+            _counter++;
+        }
+
+        Console.WriteLine("Do-While: ");
+        _counter = 0;
+        do {
+            Console.WriteLine(_counter);
+            _counter++;
+        } while (_counter < 10);
+
+        Console.WriteLine("For с фразами: ");
+        string _text = "";
+        for (int i = 0; i < 2; i++) {
+            Console.WriteLine($"Введите фразу #{i+1}");
+            _text += " " + Console.ReadLine();
+        }
+        Console.WriteLine($"Введенный текст: {_text}");
     }
 }
